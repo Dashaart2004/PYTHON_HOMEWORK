fef3b6b63fbe918467e0ca324f2cc9fa781267e6
Dashaart2004 <daraartunina881@gmail.com>
2024-04-15 18:23:33 +0300

    add first homework

---
+++ Python homework/lesson_1/lesson_1_task_6.py
@@ -0,0 +1,32 @@
+# Выведи в консоль 1
+def func1():
+    print(1, end="")
+#Вывведи в консоль 2
+def func2():
+    print(2, end="")
+#Вывведи в консоль 3
+def func3():
+    print(3, end="")
+#Вывведи в консоль 4
+def func4():
+    print(4, end="")
+#Вывведи в консоль 5
+def func5():
+    print(5, end="")
+#Вывведи в консоль 6
+def func6():
+    print(6, end="")
+#Вывведи в консоль 7
+def func7():
+    print(7, end="")
+#Вывведи в консоль 8
+def func8():
+    print(8, end="")
+#Вывведи в консоль 9
+def func9():
+    print(9, end="")
+#Вывведи в консоль 0
+def func0():
+    print(0, end="")
+
+func8() ,func8(), func0(),func0(),func5(),func5(),func5(),func3(),func5(),func3(),func5()
